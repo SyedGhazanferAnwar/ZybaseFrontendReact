5c982e105c8213dd060c54b8fd0176f6e47f2f70
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2018-12-02 20:44:22 +0500

    combox-fix

--- src/AdminPanel/CreateTableUI.jsx
+++ src/AdminPanel/CreateTableUI.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import NavBar from "./component/navBar.jsx";
-import SideMenu from "./component/sideMenu.jsx";
-import Main from "./component/main.jsx";
-import CreateTable from "./component/sidebarComponents/createTable";
+import React, {Component} from 'react';
+import NavBar from './component/navBar.jsx';
+import SideMenu from './component/sideMenu.jsx';
+import Main from './component/main.jsx';
+import CreateTable from './component/sidebarComponents/createTable';
 
 class CreateTableUI extends Component {
   constructor() {
@@ -10,7 +10,7 @@ class CreateTableUI extends Component {
     let ss = this.state.row;
   }
   state = {
-    row: ["row", "row2"]
+    row: ['row', 'row2'],
   };
 
   render() {
@@ -22,6 +22,7 @@ class CreateTableUI extends Component {
           {/* end of navBar */}
           {/* <!-- LEFT SIDEBAR --> */}
           <SideMenu callingComponent="desk" />
+
           {/* <!-- END LEFT SIDEBAR --> */}
           {/* <!-- MAIN --> */}
           {/* <CreateTable /> */}
@@ -31,7 +32,7 @@ class CreateTableUI extends Component {
           <footer>
             <div className="container-fluid">
               <p className="copyright">
-                &copy; 2017{" "}
+                &copy; 2017{' '}
                 <a href="https://www.themeineed.com" target="_blank">
                   Theme I Need
                 </a>
--- src/AdminPanel/component/sidebarComponents/comboBox.jsx
+++ src/AdminPanel/component/sidebarComponents/comboBox.jsx
@@ -1,7 +1,68 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 class ComboBox extends Component {
   state = {};
 
+  componentDidMount() {
+    /* ===== Logic for creating fake Select Boxes ===== */
+    $('.sel').each(function() {
+      $(this)
+        .children('select')
+        .css('display', 'none');
+
+      var $current = $(this);
+
+      $(this)
+        .find('option')
+        .each(function(i) {
+          if (i == 0) {
+            $current.prepend(
+              $('<div>', {
+                class: $current.attr('class').replace(/sel/g, 'sel__box'),
+              })
+            );
+
+            var placeholder = $(this).text();
+            $current.prepend(
+              $('<span>', {
+                class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
+                text: placeholder,
+                'data-placeholder': placeholder,
+              })
+            );
+
+            return;
+          }
+
+          $current.children('div').append(
+            $('<span>', {
+              class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
+              text: $(this).text(),
+            })
+          );
+        });
+    });
+
+    // Toggling the `.active` state on the `.sel`.
+    $('.sel').click(function() {
+      $(this).toggleClass('active');
+    });
+
+    // Toggling the `.selected` state on the options.
+    $('.sel__box__options').click(function() {
+      var txt = $(this).text();
+      var index = $(this).index();
+
+      $(this)
+        .siblings('.sel__box__options')
+        .removeClass('selected');
+      $(this).addClass('selected');
+
+      var $currentSel = $(this).closest('.sel');
+      $currentSel.children('.sel__placeholder').text(txt);
+      $currentSel.children('select').prop('selectedIndex', index + 1);
+    });
+  }
   render() {
     return (
       <React.Fragment>
@@ -19,18 +80,6 @@ class ComboBox extends Component {
         </div>
 
         <hr class="rule" />
-
-        <div class="sel sel--superman">
-          <select name="select-superpower" id="select-superpower">
-            <option value="" disabled>
-              Superpower
-            </option>
-            <option value="hacker">Power</option>
-            <option value="gamer">Speed</option>
-            <option value="developer">Acrobatics</option>
-            <option value="programmer">Accuracy</option>
-          </select>
-        </div>
       </React.Fragment>
     );
   }
